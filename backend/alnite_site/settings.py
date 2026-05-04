dacb70456bd39dbbaedf3131860806c3bb30e956
codex-pixel <codex10800@gmail.com>
2026-05-04 13:00:57 +0200

    switch to sqlite

--- backend/alnite_site/settings.py
+++ backend/alnite_site/settings.py
@@ -81,8 +81,14 @@ WSGI_APPLICATION = 'alnite_site.wsgi.application'
 import dj_database_url
 import os
 
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
