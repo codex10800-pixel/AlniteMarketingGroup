5e412500f99ed7154c6cb849060639131557d2ec
codex-pixel <codex10800@gmail.com>
2026-05-04 13:05:01 +0200

    fix database config for production

--- backend/alnite_site/settings.py
+++ backend/alnite_site/settings.py
@@ -81,14 +81,10 @@ WSGI_APPLICATION = 'alnite_site.wsgi.application'
 import dj_database_url
 import os
 
-from pathlib import Path
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 
